b9b0c6fe570e8bdc874c7a81f8ff29c9cb424e3a
Brenda G <bren.gonzalez1926@gmail.com>
2024-03-07 19:31:25 +0000

    min | Completed Header & Footer link/routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Link as ScrollLink } from 'react-scroll';
+import React, { useState } from 'react';
+import { animateScroll as scroll, Link } from 'react-scroll'
 import { Link as RouterLink, useLocation } from 'react-router-dom';
 import './Header.scss';
 import Logo from '../../assets/icons/Portfolio_Logo.svg';
@@ -8,30 +8,40 @@ function Header() {
     const [showMobileHeader, setShowMobileHeader] = useState(false);
     const location = useLocation();
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const heroSection = document.getElementById('hero');
-            const header = document.querySelector('.navbar');
-
-            if (heroSection && header) {
-                const heroSectionBottom = heroSection.offsetTop + heroSection.offsetHeight;
-                const scrollPosition = window.scrollY + header.offsetHeight;
-
-                setShowMobileHeader(scrollPosition > heroSectionBottom);
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
     const scrollToTop = () => {
         if (location.pathname === '/') {
             // If on the home page, scroll to the top
-            window.scrollTo(0, 0);
+            scroll.scrollToTop({ smooth: true, });
+        } else {
+            // If on another page, navigate to the home page
+            window.location.href = '/';
+        }
+    };
+
+    const scrollToAbout = () => {
+        if (location.pathname === '/') {
+            // If on the home page, scroll to the top
+            scroll.scrollTo(700, { smooth: true, });
+        } else {
+            // If on another page, navigate to the home page
+            window.location.href = '/';
+        }
+    };
+
+    const scrollToProjects = () => {
+        if (location.pathname === '/') {
+            // If on the home page, scroll to the top
+            scroll.scrollTo(2000, { smooth: true, });
+        } else {
+            // If on another page, navigate to the home page
+            window.location.href = '/';
+        }
+    };
+
+    const scrollToContact = () => {
+        if (location.pathname === '/') {
+            // If on the home page, scroll to the top
+            scroll.scrollTo(2700, { smooth: true, });
         } else {
             // If on another page, navigate to the home page
             window.location.href = '/';
@@ -43,67 +53,22 @@ function Header() {
             <header className={`navbar ${showMobileHeader ? 'show-mobile-header' : ''}`}>
                 <nav className='navbar__cont'>
                     <div className='navbar__sec'>
-                        <ScrollLink
-                            to="hero"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={500}
-                            className='navbar__links--mobile'
-                        >
-                            Home
-                        </ScrollLink>
+                        <div className='navbar__links--mobile' onClick={scrollToTop}>Home</div>
                         <div className='navbar__logo' onClick={scrollToTop}>
                             <img src={Logo} alt="Bren.Dev Logo" className='navbar__logo--desktop' />
                         </div>
-                        <ScrollLink
-                            to="contact"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={500}
-                            className='navbar__links--mobile'
-                        >
-                            Contact
-                        </ScrollLink>
+                        <div onClick={scrollToContact} className='navbar__links--mobile'>Contact</div>
                     </div>
                     <div className='navbar__sec'>
                         <ul className='navbar__opts'>
                             <li>
-                                <ScrollLink
-                                    to="aboutme"
-                                    spy={true}
-                                    smooth={true}
-                                    offset={-70}
-                                    duration={500}
-                                    className='navbar__links'
-                                >
-                                    Who
-                                </ScrollLink>
+                                <div onClick={scrollToAbout} className='navbar__links'>Who</div>
                             </li>
                             <li>
-                                <ScrollLink
-                                    to="projects"
-                                    spy={true}
-                                    smooth={true}
-                                    offset={-70}
-                                    duration={500}
-                                    className='navbar__links'
-                                >
-                                    What
-                                </ScrollLink>
+                                <div onClick={scrollToProjects} className='navbar__links' >What</div>
                             </li>
                             <li>
-                                <ScrollLink
-                                    to="contact"
-                                    spy={true}
-                                    smooth={true}
-                                    offset={-70}
-                                    duration={500}
-                                    className='navbar__links'
-                                >
-                                    Where
-                                </ScrollLink>
+                                <div onClick={scrollToContact} className='navbar__links'>Where</div>
                             </li>
                         </ul>
                         <div className='navbar__opts'>
